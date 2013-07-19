a2fb9ae731b947891e5c936fb0d83db02651fa28
Livien Yin <LivienYin@gmail.com>
2013-07-18 19:54:14 -0700

    Includes tablesort.min.js

--- table.js
+++ table.js
@@ -1,3 +1,15 @@
- sheet = function($scope) {
-    $scope.cell = "hello spreadsheet!";
+angular.module('alation-tables', []).directive(function() {
+  return {
+    scope: {
+      columnNames: "=columnNames",
+      
+    }
   }
+});
+
+function TableCtrl($scope) {
+  $scope.columns = ['Attributes', 'Entity Type', 'Physical Type', 'Data Type'];
+  $scope.rows = ['Month', 'Days', 'Year', 'Customer ID', 'Product', 'Quantity', 'Channel', 'Cashier'];
+  $scope.cells = {};
+}
+
